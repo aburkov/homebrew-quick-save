c36696fec571fc237e6ae9903949eff710a38766
Andriy Burkov <a.burkov@gmail.com>
2025-05-31 18:42:53 -0400

    quick-save 0.0.1 – fix URL and sha256

--- Formula/quick-save.rb
+++ Formula/quick-save.rb
@@ -2,7 +2,7 @@ class QuickSave < Formula
   desc     "Ultra‑simple save / load wrapper around Git"
   homepage "https://github.com/aburkov/quick-save"
   url      "https://github.com/aburkov/quick-save/archive/refs/tags/v0.0.1.tar.gz"
-  sha256   "<PASTE_HASH_HERE>"
+  sha256   "c47967dae3a010cf7f3372094b33b0f7f95cff56d89ce05d85184d996ce0ef88"
   license  "MIT"
 
   depends_on "git"
